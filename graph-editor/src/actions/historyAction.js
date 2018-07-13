bcbfd8ba36dc46a7ce05940032985cd5e46b8e2c
Intern01 <intern01@opals-MacBook-Pro.local>
2018-07-13 15:51:09 +0700

    Add fundamental function via saga and connect console to database

---
+++ graph-editor/src/actions/historyAction.js
@@ -0,0 +1,16 @@
+const displayHistory = ()=>({
+    type: 'DISPLAY_HISTORY'
+  
+})
+
+const undisplayHistory = () => ({
+    type: 'UNDISPLAY_HISTORY'
+
+})
+
+export {displayHistory,
+    undisplayHistory
+}
+
+
+
