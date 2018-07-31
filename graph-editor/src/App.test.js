1796f63dddd33ce64e26b37773428c4da04fdc9d
Intern01 <intern01@opals-MacBook-Pro.local>
2018-07-31 16:14:31 +0700

    Fix App cannot find store in App.test.js

--- graph-editor/src/App.test.js
+++ graph-editor/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+import store from "./stores/store";
+import { Provider } from "react-redux";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
